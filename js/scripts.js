ff50e112944484d35bd498c1e02d0335a8fa3a9b
natalia-al-shevchik <mc.nataki@gmail.com>
2025-10-26 16:06:23 +0100

    Add interactive responsive navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hey there! Welcome to my creative world! I'm thrilled to see you here")
+function menuToggle() {
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
+  }
+}
